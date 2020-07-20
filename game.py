46d7abeb436e93f497dd6816b1d518416ed3aefa
Jake Acquadro <jakeacquadro@Jakes-MacBook-Air.local>
2020-07-20 14:55:47 -0400

    Fixed menu labels

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_menu
 from Paddle import Paddle
 from Human import Human
 from Ball import Ball
@@ -10,137 +11,173 @@ from pygame.locals import (
     K_a,
     K_z
 )
+class Game:
 
-# two gamemodes: 1 is Player vs Player, 2 is Computer vs Player
-gamemode = 2
+    # constructor (runs game)
+    def __init__(self):
+        # initialize variables
+        self.gamemode = 1
+        self.difficulty = 1
 
-# initialize the display module
-pygame.init()
-pygame.display.set_caption('Pong')
+        self.screen_width = 700
+        self.screen_height = 400
 
-# create window
-screen_width = 700
-screen_height = 400
+        # initialize the display module
+        pygame.init()
+        pygame.display.set_caption('Pong')
 
-# screen is a Surface object
-screen = pygame.display.set_mode([screen_width, screen_height])
+        # screen is a Surface object
+        self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
 
-# fill screen with black
-screen.fill((0,0,0))
+        # create menu
+        menu = pygame_menu.Menu(self.screen_height - 100, self.screen_width - 100, 'Pong',\
+            theme=pygame_menu.themes.THEME_DARK)
+        menu.add_selector('Difficulty:', [('Beginner', 1), ('Intermediate', 2), ('Expert', 3)], onchange= self.set_difficulty)
+        menu.add_selector('Gamemode:', [('Player vs Player', 1), ('Computer vs Player', 2)], onchange= self.set_gamemode)
+        menu.add_button('Play', self.game_loop)
+        menu.add_button('Quit', pygame_menu.events.EXIT)
+        menu.mainloop(self.screen)
 
-# add scoreboard
-font = pygame.font.Font('freesansbold.ttf', 32)
-
-score1 = 0
-score1_text = font.render(str(score1), True, (255,255,255))
-score1_rect = score1_text.get_rect()
-
-score2 = 0
-score2_text = font.render(str(score2), True, (255,255,255))
-score2_rect = score2_text.get_rect()
-
-score1_rect.center = (screen_width/2 - 50, 20)
-score2_rect.center = (screen_width/2 + 50, 20)
-
-# add two paddles
-paddle_width = 20
-paddle_height = 110
-
-# paddle starting coordinates
-init_x_1 = 5*screen_width/6 - paddle_width/2
-init_y_1 = screen_height/2 - paddle_height/2
-
-init_x_2 = screen_width/6 - paddle_width/2
-init_y_2 = screen_height/2- paddle_height/2
-
-# create paddles
-right_paddle = Paddle(paddle_width, paddle_height, init_x_1, init_y_1)
-left_paddle = Paddle(paddle_width, paddle_height, init_x_2, init_y_2)
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit()
 
 
-# create first paddle
-right_paddle.rect.x = init_x_1 # update position of paddle
-right_paddle.rect.y = init_y_1
-screen.blit(right_paddle.surf, (init_x_1, init_y_1)) # put paddle on screen
 
-# create second paddle
-left_paddle.rect.x = init_x_2 # update position of paddle
-left_paddle.rect.y = init_y_2
-screen.blit(left_paddle.surf, (init_x_2, init_y_2))
+    # sets self.difficulty
+    def set_difficulty(self,value,diff):
+        self.difficulty = diff
 
-# create human characters
-human_1 = Human(right_paddle, screen_height, K_UP, K_DOWN)
-# create second human if necessary
-if gamemode == 1:
-    human_2 = Human(left_paddle, screen_height, K_a, K_z)
-# create computer if necessary
-elif gamemode == 2:
-    distance = 40 # number of pixels to react to ball
-    computer = Computer(left_paddle, screen_height, distance)
+    # sets self.gamemode where 1 is Player vs Player, 2 is Computer vs Player
+    def set_gamemode(self,value,mode):
+        self.gamemode = mode
 
-# create ball
-ball_len = 10
-init_x_ball = screen_width/2
-init_y_ball = screen_height/2
-speed = 6
-ball = Ball(ball_len, init_x_ball, init_y_ball, speed, screen_height, screen_width)
-screen.blit(ball.surf, (init_x_ball, init_y_ball)) # put ball on screen
+    def game_loop(self):
+        if self.difficulty == 1:
+            speed = 5
+            distance = 30
+        elif self.difficulty == 2:
+            speed = 6
+            distance = 40
+        elif self.difficulty == 3:
+            speed = 8
+            distance = 40
 
-# keep game running until closed by user
-on = True
+        # fill screen with black
+        self.screen.fill((0,0,0))
 
-# as long as this while loop is running (and by extension, the program),
-# the window stays open
-while on:
+        # add scoreboard
+        font = pygame.font.Font('freesansbold.ttf', 32)
 
-    # check for quit by user
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            on = False
-
-    # update background before showing other objects
-    screen.fill((0,0,0))
-
-
-    # check for key press
-    pressed_keys = pygame.key.get_pressed()
-
-    # if key pressed, move paddle
-    human_1.update(pressed_keys)
-    if gamemode == 1:
-        human_2.update(pressed_keys)
-    elif gamemode == 2:
-        computer.update(ball.rect.x, ball.rect.y)
-
-    # check for collision
-    if pygame.sprite.collide_rect(ball, right_paddle):
-        ball.reverse()
-    elif pygame.sprite.collide_rect(ball, left_paddle):
-        ball.reverse()
-
-    if ball.rect.right <= 0:
-        ball.reset()
-        ball.x_speed = ball.init_speed
-        score2 += 1
-        score2_text = font.render(str(score2), True, (255,255,255))
-    if ball.rect.left >= ball.screen_width:
-        ball.reset()
-        ball.x_speed = -ball.init_speed
-        score1 += 1
+        score1 = 0
         score1_text = font.render(str(score1), True, (255,255,255))
+        score1_rect = score1_text.get_rect()
 
-    # update ball position
-    ball.update()
+        score2 = 0
+        score2_text = font.render(str(score2), True, (255,255,255))
+        score2_rect = score2_text.get_rect()
 
-    # update graphics
-    screen.blit(right_paddle.surf, right_paddle.rect)
-    screen.blit(left_paddle.surf, left_paddle.rect)
-    screen.blit(ball.surf, ball.rect)
-    screen.blit(score1_text, score1_rect)
-    screen.blit(score2_text, score2_rect)
+        score1_rect.center = (self.screen_width/2 - 50, 20)
+        score2_rect.center = (self.screen_width/2 + 50, 20)
 
-    # flip (refresh) the screen
-    pygame.display.flip()
+        # add two paddles
+        paddle_width = 20
+        paddle_height = 110
+
+        # paddle starting coordinates
+        init_x_1 = 5*self.screen_width/6 - paddle_width/2
+        init_y_1 = self.screen_height/2 - paddle_height/2
+
+        init_x_2 = self.screen_width/6 - paddle_width/2
+        init_y_2 = self.screen_height/2- paddle_height/2
+
+        # create paddles
+        right_paddle = Paddle(paddle_width, paddle_height, init_x_1, init_y_1)
+        left_paddle = Paddle(paddle_width, paddle_height, init_x_2, init_y_2)
 
 
-pygame.quit()
+        # create first paddle
+        right_paddle.rect.x = init_x_1 # update position of paddle
+        right_paddle.rect.y = init_y_1
+        self.screen.blit(right_paddle.surf, (init_x_1, init_y_1)) # put paddle on screen
+
+        # create second paddle
+        left_paddle.rect.x = init_x_2 # update position of paddle
+        left_paddle.rect.y = init_y_2
+        self.screen.blit(left_paddle.surf, (init_x_2, init_y_2))
+
+        # create human characters
+        human_1 = Human(right_paddle, self.screen_height, K_UP, K_DOWN)
+        # create second human if necessary
+        if self.gamemode == 1:
+            human_2 = Human(left_paddle, self.screen_height, K_a, K_z)
+        # create computer if necessary
+        elif self.gamemode == 2:
+            computer = Computer(left_paddle, self.screen_height, distance)
+
+        # create ball
+        ball_len = 10
+        init_x_ball = self.screen_width/2
+        init_y_ball = self.screen_height/2
+        ball = Ball(ball_len, init_x_ball, init_y_ball, speed, self.screen_height, self.screen_width)
+        self.screen.blit(ball.surf, (init_x_ball, init_y_ball)) # put ball on screen
+
+
+        # keep game running until closed by user
+        on = True
+
+        # as long as this while loop is running (and by extension, the program),
+        # the window stays open
+        while on:
+
+            # check for quit by user
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    on = False
+
+            # update background before showing other objects
+            self.screen.fill((0,0,0))
+
+
+            # check for key press
+            pressed_keys = pygame.key.get_pressed()
+
+            # if key pressed, move paddle
+            human_1.update(pressed_keys)
+            if self.gamemode == 1:
+                human_2.update(pressed_keys)
+            elif self.gamemode == 2:
+                computer.update(ball.rect.x, ball.rect.y)
+
+            # check for collision
+            if pygame.sprite.collide_rect(ball, right_paddle):
+                ball.reverse()
+            elif pygame.sprite.collide_rect(ball, left_paddle):
+                ball.reverse()
+
+            if ball.rect.right <= 0:
+                ball.reset()
+                ball.x_speed = ball.init_speed
+                score2 += 1
+                score2_text = font.render(str(score2), True, (255,255,255))
+            if ball.rect.left >= ball.screen_width:
+                ball.reset()
+                ball.x_speed = -ball.init_speed
+                score1 += 1
+                score1_text = font.render(str(score1), True, (255,255,255))
+
+            # update ball position
+            ball.update()
+
+            # update graphics
+            self.screen.blit(right_paddle.surf, right_paddle.rect)
+            self.screen.blit(left_paddle.surf, left_paddle.rect)
+            self.screen.blit(ball.surf, ball.rect)
+            self.screen.blit(score1_text, score1_rect)
+            self.screen.blit(score2_text, score2_rect)
+
+            # flip (refresh) the screen
+            pygame.display.flip()
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit()
